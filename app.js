5a17db20a576c2827fd21b0c386e342974573b94
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-07-14 15:36:54 +0800

    加入JSONparse

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const request = require('request');
 const disqusPublicKey = 'o25Rq85nLUWhQ034GnQ8ny8ENNRPi74msQ0TCpBHZbVohG7PBP41vJEQp5sRyAJ9';
 const disqusSecretKey = 'ItYRkmy3RpxvhPESeSbKW4u4SRZp9iesVnNd2Mq2WlRCIvu3tiRmj0EkVZZxcFMf';
 const disqusForum = 'renthouserobot';
+const disqusForumAddress = 'https://disqus.com/api/3.0/forums';
 const port = process.env.PORT || 80;
 
 app.get('/', function (req, res) {
-  request('https://disqus.com/api/3.0/forums/listThreads.json?forum=' + disqusForum + '&api_key=' + disqusPublicKey, function (error, response, body) {
-    res.send(body);
+  request(disqusForumAddress + '/listThreads.json?forum=' + disqusForum + '&api_key=' + disqusPublicKey, function (error, response, body) {
+    let JSONparse = JSON.parse(body);
+    res.send(JSONparse);
   });
 });
 
